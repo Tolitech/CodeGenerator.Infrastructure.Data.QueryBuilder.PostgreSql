4d669ec44de13a08d5007f1dfce6b7f09f1e3b3f
Eduardo Tolino <eduardo.tolino@tolitech.com.br>
2022-02-14 14:11:02 -0300

    initial implementaion.

---
+++ src/QueryBuilder.PostgreSql/SqlBuilderConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tolitech.CodeGenerator.Infrastructure.Data.QueryBuilder.PostgreSql
+{
+    public class SqlBuilderConfiguration
+    {
+        public static void UsePostgreSql()
+        {
+            AddQueryBuilder("default");
+        }
+
+        public static void AddQueryBuilder(string key)
+        {
+            var builderFactory = new PostgreSqlBuilderFactory();
+            SqlBuilder.AddQueryBuilder(key, builderFactory);
+        }
+    }
+}
---
+++ src/QueryBuilder.PostgreSql/SqlBuilderInsert.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Tolitech.CodeGenerator.Infrastructure.Data.QueryBuilder.PostgreSql
+{
+    public class SqlBuilderInsert : QueryBuilder.SqlBuilderInsert
+    {
+        public SqlBuilderInsert(IBuilderFactory builderFactory, string schemaName, string tableName) : base(builderFactory, schemaName, tableName)
+        {
+
+        }
+
+        public override string Build()
+        {
+            string sql = $"insert into \"{schemaName}\".\"{tableName}\" {GetSqlColumnsToInsert()}";
+
+            if (isIdentity)
+                sql += $" returning \"{identityColumnName}\";";
+            else
+                sql += ";";
+
+            return sql;
+        }
+
+        private string GetSqlColumnsToInsert()
+        {
+            IList<string> columnsRenamed = new List<string>();
+            foreach (var column in base.columns)
+                columnsRenamed.Add($"\"{base.GetColumnName(column)}\"");
+
+            string sql = "(" + string.Join(", ", columnsRenamed) + ")";
+            sql += " values ";
+            sql += "(@" + string.Join(", @", base.columns) + ")";
+            return sql;
+        }
+    }
+}
